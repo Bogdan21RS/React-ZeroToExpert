1832b700fa10194a61986693f02736991dd536b5
Bogdan <razvant.153957@e.unavarra.es>
2025-07-18 09:55:29 +0200

    Created components with typing using PropTypes

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,20 @@
-export default function FirstApp() {
+import PropTypes from "prop-types";
+
+export default function FirstApp({ title, subtitle }) {
   return (
     <>
-      <h1>First App</h1>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
       <p>This is a paragraph</p>
     </>
   );
 }
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.number,
+};
+
+FirstApp.defaultProps = {
+  subtitle: 123,
+};
