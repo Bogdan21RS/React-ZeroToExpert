d123833dd20b7df6413af58599d1a8e8dcfcb043
Bogdan <razvant.153957@e.unavarra.es>
2025-07-25 09:36:43 +0200

    Ended introduction react course

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function CounterApp({ value }) {
+  const [counter, setCounter] = useState(value);
+
+  function handleAdd() {
+    // setCounter( (counter) => counter + 1)
+    setCounter(counter + 1);
+  }
+
+  function handleSubstract() {
+    setCounter(counter - 1);
+  }
+
+  function handleReset() {
+    setCounter(value);
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
+
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubstract}>-1</button>
+      <button onClick={handleReset}>reset</button>
     </>
   );
 }
